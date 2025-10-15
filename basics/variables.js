a642f9ff46b5b756b075f5bb0db95bf66487f68a
Anuj singh <anujsinghkaushalpuri@gmail.com>
2025-10-15 11:16:15 +0000

    container config done

---
+++ basics/variables.js
@@ -0,0 +1,9 @@
+console.log("anuj")
+const accountid = "243544"
+let accountgmail = "anujsiongh@"
+let accounthousename = "kaushalpuricolony"
+accountgmail = "anujjkdfibivb"
+accounthousename = "ayodhya"
+
+console.table([accounthousename,accountgmail]);
+

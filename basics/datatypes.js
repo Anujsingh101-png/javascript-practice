802d14b6137ccb597b8adf683e10de52ab83bb6f
Anuj singh <anujsinghkaushalpuri@gmail.com>
2025-10-15 13:25:42 +0000

    datatypes

---
+++ basics/datatypes.js
@@ -0,0 +1,18 @@
+"use strict" // treat all js code as a newer version
+console.log(3*3) //code readibility should be high
+
+let name = "anuj"
+let age = 23
+let isdead = false
+let state 
+
+// number 2 to the power 53
+// bigint
+// string => " "
+// boolen => true/false
+// null => stand alonevalue
+// undefined => let state
+// symbol => unique
+// objects
+console.log(typeof null); //  object
+console.log(typeof undefined); //same

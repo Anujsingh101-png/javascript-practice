8bdf6c26ab211ffb8ee1213df97ca16e7744c18e
Anuj singh <anujsinghkaushalpuri@gmail.com>
2025-10-15 14:22:16 +0000

    conversionoperation

--- basics/conversionoperation.js
+++ basics/conversionoperation.js
@@ -18,3 +18,35 @@ console.log(bool)
 //"anuj" => true
 
 // same goes for string
+
+// operations
+console.log("for operations")
+
+let negval = -3
+console.log(negval)
+
+
+console.log(2+2)
+console.log(2-2)
+console.log(2**2)// power of 2
+// ---
+let str1 = "anuj"
+let str = "singh"
+console.log(str1 + str)
+
+
+console.log("1" + 2)
+
+console.log(3 + "3")
+
+console.log("1" + 2 + 4)
+console.log(1 + 2 + "4")
+// For please be professional dont use messi code just for output write code in standared and clean version
+console.log((1 + 2) + 76)
+
+let gamecounter = 100
+let gamecounter1 = 100
+++gamecounter;
+console.log(gamecounter)
+gamecounter1++;
+console.log(gamecounter1)

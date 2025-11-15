4e05eec91bd799d3238795c9fbc476a811312275
Anuj singh <anujsinghkaushalpuri@gmail.com>
2025-11-15 20:36:41 +0000

    IIFE

---
+++ 03_basics/04_IIFE.js
@@ -0,0 +1,9 @@
+ // Immediately Invoked Function Expressions IIFE
+ 
+ (function chai(){
+  console.log("anuj singh") // named fuction
+ })();
+
+((name) =>{
+    console.log(`my name is ${name}`)
+})("anuj singh")
